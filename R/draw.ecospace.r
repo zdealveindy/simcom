e7b781a174a0872d65a93362b49655f32e153ecf
David Zelený <zdealveindy@users.noreply.github.com>
2025-08-09 23:28:25 +0800

    update to new version

--- R/draw.ecospace.r
+++ R/draw.ecospace.r
@@ -1,10 +1,10 @@
-#' Draws ecospace with species response surface for two dimensional simulated data
+#' Draws ecospace with species response surface for two dimensional data simulated and sampled by simul.comm and sample.comm.
 #' @author David Zeleny (zeleny.david@@gmail.com)
-#' @param sampled.comm Object with simulated community data created by \code{sample.comm.2} function.
+#' @param saco Object with simulated community data created by \code{sample.comm.2} function.
+#' @param plot.grad List gradients which should be plotted by the function.
 #' @param resolution Number of items drawn vertically and horizontally. Default = 200.
 #' @param colors Vector of hexadecimal strings of colors used to draw individual species response surfaces. Each element should be 7 characters long, starting with \code{"#"} (e.g. \code{"#8DD3C7"}). Default is a vector of 12 colors created by pallete "Set3" from \code{library (RColorBrewer)}.
 #' @param species Vector with ID's of species drawn into plot. Default = NULL, which means all species are drawn.
-#' @param sort Should be species sorted before drawing, so as the species with largest area in the plot are drawn first? Default = TRUE.
 #' @param asp Aspect ratio of the plotted region (see argument \code{asp} in \code{\link{plot.window}}).
 #' @param sample.pch,sample.cex Plotting character and its size for points showing location of samples in ecospace. See \code{pch} and \code{cex} in function \code{\link{points}}.
 #' @param species.pch,species.cex Plotting character and its size for points forming the species response surfase in the ecospace. See \code{pch} and \code{cex} in function \code{\link{points}}.
@@ -12,39 +12,39 @@
 #' @param box Logical; draw the box around plotting region?
 #' @param axes Logical; draw axes?
 #' @param ... Other arguments passed into the function \code{title}.
-#' @seealso \code{\link{simul.comm.2}}, \code{\link{sample.comm.2}}
-#' @examples
-#' draw.ecospace (sample.comm.2 (simul.comm.2 (totS = 100)), resolution = 100)
 #' @rdname draw.ecospace
 #' @export
-draw.ecospace <- function (sampled.comm, resolution = 200, colors = NULL, species = NULL, sort = TRUE, asp = 1, sample.pch = 3, sample.cex = .5, species.pch = 16, species.cex = 1, xlab = 'gradient 1', ylab = 'gradient 2', axes = T, box = T, ...)
+draw.ecospace <- function (saco, plot.grad = c(1,2), resolution = 200, colors = NULL, species = NULL, asp = NULL, sample.pch = 3, sample.cex = .5, species.pch = 16, species.cex = 1, xlab = 'gradient 1', ylab = 'gradient 2', axes = TRUE, box = TRUE, ...)
 {
+  
   if (is.null (colors)) colors <- RColorBrewer::brewer.pal (12, 'Set3')
-  if (is.null (species)) species <- 1:sampled.comm$simul.comm$totS
-  sc <- sampled.comm$simul.comm
-  #windows ()
-  plot.new ()
-  plot.window (xlim = c(0, sc$gr1.length), ylim = c(0, sc$gr2.length), asp = asp)
-  if (axes) axis (1)
-  if (axes) axis (2)
-  if (box) box ()
-  title (xlab = xlab, ylab = ylab, ...)
-  gr1gr2 <- expand.grid (gr1 = round (seq (1, sc$gr1.length, length = resolution)), gr2 = round (seq (1, sc$gr2.length, length = resolution)))
-  spec.prob <- sapply (1:ncol (sc$A1.all), FUN = function (sp) sc$A1.all[gr1gr2$gr1,sp]*sc$A2.all[gr1gr2$gr2, sp])
-  sorted.species <- order (colSums (spec.prob[,species] > 0), decreasing = T)
+  if (is.null (species)) species <- 1:saco$S
+  
+  graphics::plot.new ()
+  graphics::plot.window (xlim = c(0, resolution), ylim = c(0, resolution), asp = asp)
+  graphics::title (xlab = xlab, ylab = ylab) #, ...)  # later return back
+  
+  gr1 <- round (seq (1, saco$args.simcom$gr.length[saco$grad[1]], length = resolution))
+  gr2 <- round (seq (1, saco$args.simcom$gr.length[saco$grad[2]], length = resolution))
+  spec.prob <- lapply (species, FUN = function (sp) saco$args.simcom$Ao[sp]*outer (saco$args.simcom$A.all[[1]][gr1, sp], saco$args.simcom$A.all[[2]][gr2, sp], `*`))
+  sorted.species <- order (saco$args.simcom$niche[species], decreasing = TRUE)
   colors.all <- rep (colors, length.out = length (species))
+  
   for (sp in sorted.species)
   {
-    if (sum (spec.prob[,sp] > 0)>0)
+    if (sum (spec.prob[[sp]] > 0)>0)
     {
-      lower.bound <- quantile (spec.prob[spec.prob[,sp]>0,sp], prob = .1)
-      points.coords <- gr1gr2[spec.prob[,sp] > lower.bound,]
-      species.prob <- spec.prob[spec.prob[,sp] > lower.bound,sp]
+      lower.bound <- stats::quantile (spec.prob[[sp]][spec.prob[[sp]]>0], prob = .5)
+      points.coords <- which (spec.prob[[sp]] > lower.bound, arr.ind = TRUE)
+      species.prob <- spec.prob[[sp]][spec.prob[[sp]] > lower.bound]
       species.prob.ranged <- (species.prob - min (species.prob))/(max (species.prob) - min (species.prob))*255
       colors.temp <- paste (colors.all[sp], format (as.hexmode (round (species.prob.ranged)), upper = T), sep = '')
-      points (points.coords, col = colors.temp, pch = species.pch, cex = species.cex)
+      graphics::points (points.coords, col = colors.temp, pch = species.pch, cex = species.cex)
     }
-
   }
-  points (sampled.comm$sample.x1, sampled.comm$sample.x2, pch = sample.pch, cex = sample.cex)
+  graphics::plot.window (xlim = c(0, saco$args.simcom$gr.length[saco$grad[1]]), ylim = c(0, saco$args.simcom$gr.length[saco$grad[2]]), asp = asp)
+  points (as.data.frame (saco$sample.x), pch = sample.pch, cex = sample.cex)
+  if (axes) graphics::axis (1)
+  if (axes) graphics::axis (2)
+  if (box) graphics::box ()
 }
---
+++ R/simcom-package.R
@@ -0,0 +1,6 @@
+#' simcom: simulation of community ecology dataset
+
+#' @keywords internal
+#' @importFrom graphics axis lines par points plot.new
+#' @importFrom stats quantile rbinom rexp rlnorm rnorm runif
+#' @name NULL
